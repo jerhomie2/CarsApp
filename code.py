d6e46c509a39de0ab90f9f1d30ba69251042d3ca
Jarom Asher <jaromasher@gmail.com>
2024-11-19 15:44:26 -0700

    first tab

--- code.py
+++ code.py
@@ -14,7 +14,7 @@ st.title('Auctioned Cars Data')
 tab1, tab2, tab3 = st.tabs(['Make','Model','Color'])
 with tab1:
     input_make = st.text_input('Enter a make:', value='CHEVROLET')
-    make_df = df[df['make'] == input_make].copy()
+    make_df = df[df['Make'] == input_make].copy()
     fig1 = px.line(make_df, x='VehYear', y='MMRCurrentRetailCleanPrice')
     st.plotly_chart(fig1)
 
