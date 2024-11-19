d91974aed37be6133cf88f86438f88531cba3d99
Jarom Asher <jaromasher@gmail.com>
2024-11-19 15:43:35 -0700

    first tab

--- code.py
+++ code.py
@@ -10,3 +10,13 @@ from plotly.subplots import make_subplots
 df = pd.read_csv('auctioncars.csv')
 
 st.title('Auctioned Cars Data')
+
+tab1, tab2, tab3 = st.tabs(['Make','Model','Color'])
+with tab1:
+    input_make = st.text_input('Enter a make:', value='CHEVROLET')
+    make_df = df[df['make'] == input_make].copy()
+    fig1 = px.line(make_df, x='VehYear', y='MMRCurrentRetailCleanPrice')
+    st.plotly_chart(fig1)
+
+with tab2:
+    input_model = st.text_input('Enter a model:', value='IMPALA')
