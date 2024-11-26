28374f938d01cb16e526a9bd9e092b231274db9b
Jarom Asher <jaromasher@gmail.com>
2024-11-26 12:22:52 -0700

    tab1

--- code.py
+++ code.py
@@ -7,23 +7,16 @@ from io import BytesIO
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-df = pd.read_csv('auctioncars.csv')
+df = pd.read_csv('sleep.csv')
 
-st.title('Auctioned Cars Data')
+st.title('Sleep Data')
 
-tab1, tab2, tab3 = st.tabs(['Make','Model','Color'])
+tab1, tab2 = st.tabs(['Age','Gender'])
 with tab1:
-    input_make = st.text_input('Enter a make (ALL CAPS):', value='CHEVROLET')
-    input_model = st.text_input('Enter a model (ALL CAPS):', value='IMPALA')
-    df1 = df[df['Make'] == input_make & df['Model'] == input_model].copy()
-    fig1 = px.line(df1, x='VehYear', y='MMRCurrentRetailCleanPrice')
+    input_age = st.slider('Age', min_value=27, max_value=59, value=40)
+    df1 = df[df['Age'] == input_age].copy()
+    fig1 = px.line(df1, x='Age', y='MMRCurrentRetailCleanPriceSleep Duration')
     st.plotly_chart(fig1)
 
 with tab2:
-    input_model = st.text_input('Enter a model:', value='IMPALA')
-
-with tab3:
-    input_color = st.text_input('Enter a color(ALL CAPS):', value='RED')
-    df3 = df[df['Color'] == input_color].copy()
-    fig3 = px.line(df3, x='VehYear', y='MMRCurrentRetailCleanPrice')
-    st.plotly_chart(fig3)
+    st.text('Hello')
